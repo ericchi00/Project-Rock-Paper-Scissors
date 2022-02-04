43742fc02471a59a05f22923326aafb6e22b429a
ericchi00 <ericchi00@gmail.com>
2022-02-04 09:58:19 -0800

    added function to play a round

--- script.js
+++ script.js
@@ -12,7 +12,29 @@ function computerPlay() {
 }
 
 function getRandomInt() {
-    return Math.ceil(Math.random() * (3 - 0));
+    return Math.ceil(Math.random() * 3);
 }
 
-alert(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection.toLowerCase();
+    if (playerChoice === computerChoice) {
+        return ("You tied.");
+    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+        return ("You lose! Paper beats Rock.");
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        return ("You win! Rock beats Sciscors.");
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        return ("You win! Paper beats Rock.");
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        return ("You lose! Scissors beat rock.");
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+        return ("You lose! Rock beats scissors.");
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        return ("You win! Scissors beats paper.");
+    }
+}
+
+    const playerSelection = "paper";
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
